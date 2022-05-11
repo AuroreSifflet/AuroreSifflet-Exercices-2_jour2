66b9901086bb6216652c51fffa9d6aef4f68b59b
Auroresifflet <aurore.sifflet@gmail.com>
2022-05-11 11:25:03 +0200

    Mon deuxième exercice, image de rover 3/4  et texte

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.view1}>
+        <Image style={styles.image} source={require("./rover.jpg")} />
+      </View>
+      <View style={styles.view2}>
+        <Text>Le rover Opportunity</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +17,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+  },
+  view1: {
+    flex: 3,
+  },
+  view2: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: 700,
   },
 });
